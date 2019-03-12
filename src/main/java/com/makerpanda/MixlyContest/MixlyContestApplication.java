9cb88a5d9c87a6d3bc6cba61059ef529798be148
易成林 <271614896@qq.com>
2019-03-12 17:01:41 +0800

    项目构建

---
+++ src/main/java/com/makerpanda/MixlyContest/MixlyContestApplication.java
@@ -0,0 +1,13 @@
+package com.makerpanda.MixlyContest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixlyContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixlyContestApplication.class, args);
+	}
+
+}
