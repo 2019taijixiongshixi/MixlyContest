fa321f3dbab0e236c21e5a5727e41227c191d523
易成林 <271614896@qq.com>
2019-03-29 17:00:21 +0800

    teacher更新service

--- src/main/java/com/makerpanda/MixlyContest/service/teacherservice/TeacherUpdateService.java
+++ src/main/java/com/makerpanda/MixlyContest/service/teacherservice/TeacherUpdateService.java
@@ -2,27 +2,37 @@ package com.makerpanda.MixlyContest.service.teacherservice;
 
 import com.makerpanda.MixlyContest.dao.TeacherDAO;
 import com.makerpanda.MixlyContest.datamodel.Teacher;
+import com.makerpanda.MixlyContest.service.verificationcodeservice.VerificationCodeService;
 
 public class TeacherUpdateService {
     /**
-     * 教师信息修改函数。
-     * @param teacher 新的教师对象。
-     * @return 注册成功返回0,验证失败返回1,邮箱已注册返回2,插入失败返回3
+     * 教师密码修改函数。
+     * @param newPassword 新的密码。
+     * @param email 邮箱。
+     * @return 修改成功返回0,验证失败返回1,找不到邮箱返回2,更新失败返回3
      */
-    public static int TeacherUpdateInfo (Teacher teacher, String verificationcode) {
+    public static int TeacherUpdatePassword (String verificationcode,
+                                             String newPassword,String email) {
         TeacherDAO teacherdao=new TeacherDAO();
-
+        if(!VerificationCodeService.compareVerificationCode(verificationcode))
+            return 1;
+        if(!TeacherRegisterService.checkTeacherEmail(email))
+            return 2;
+        Integer teacherid=teacherdao.getTeacherIDByTeacherEmail(email);
+        if(!teacherdao.updateTeacherPassword(newPassword,teacherid))
+            return 3;
         return 0;
     }
 
     /**
      * 教师信息修改函数。
      * @param teacher 新的教师对象。
-     * @return 注册成功返回0,验证失败返回1,邮箱已注册返回2,插入失败返回3
+     * @return 修改成功返回0,修改失败返回1
      */
-    public static int TeacherUpdatePassword (String Password) {
+    public static boolean TeacherUpdateInfo (Teacher teacher) {
         TeacherDAO teacherdao=new TeacherDAO();
-
-        return 0;
+        return teacherdao.updateTeacherInfo(teacher);
     }
+
+
 }
