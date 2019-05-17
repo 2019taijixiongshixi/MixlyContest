e28f21e2658a9c92782e7760a19391d5e4e8957e
易成林 <271614896@qq.com>
2019-05-17 10:24:49 +0800

    删除模板后端

--- src/main/java/com/makerpanda/MixlyContest/action/PageSkippingContoller.java
+++ src/main/java/com/makerpanda/MixlyContest/action/PageSkippingContoller.java
@@ -17,24 +17,6 @@ public class PageSkippingContoller {
     public String getmima(Model model) {
         model.addAttribute("teacher",new Teacher());
         model.addAttribute("student",new Student());
-        return "login/getmima";}
-    @RequestMapping(path={"/dashboard"})
-    public String dashboard(Model model) {return "master/examples/dashboard.html";}
-    @RequestMapping(path={"/icons"})
-    public String icons(Model model) {return "master/examples/icons.html";}
-    @RequestMapping(path={"/map"})
-    public String map(Model model) {return "master/examples/map.html";}
-    @RequestMapping(path={"/notifications"})
-    public String notifications(Model model) {return "master/examples/notifications.html";}
-    @RequestMapping(path={"/tables"})
-    public String tables(Model model) {return "master/examples/tables.html";}
-    @RequestMapping(path={"/typography"})
-    public String typography(Model model) {return "master/examples/typography.html";}
-    @RequestMapping(path={"/upgrades"})
-    public String upgrades(Model model) {return "master/examples/upgrades.html";}
-    @RequestMapping(path={"/user"})
-    public String user(Model model) {return "master/examples/user.html";}
-    @RequestMapping(path={"/xinxi55"})
-    public String xinxi55(Model model) {return "tijiao/xinxi55.html";}
-
+        return "login/getmima";
+    }
 }
