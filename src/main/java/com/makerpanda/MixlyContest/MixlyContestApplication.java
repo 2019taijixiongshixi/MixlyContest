d23034f7c6f80a45baab4abf0e8e637b0b2a5fe7
易成林 <271614896@qq.com>
2019-05-21 09:12:38 +0800

    最终更新

--- src/main/java/com/makerpanda/MixlyContest/MixlyContestApplication.java
+++ src/main/java/com/makerpanda/MixlyContest/MixlyContestApplication.java
@@ -2,12 +2,15 @@ package com.makerpanda.MixlyContest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MixlyContestApplication {
+public class MixlyContestApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+
 		SpringApplication.run(MixlyContestApplication.class, args);
 	}
-
 }
